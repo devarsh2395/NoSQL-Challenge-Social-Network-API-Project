4ec26687eb9dae8cb71907527c434a12b97ace4f
devarsh2395 <devarsh2395@gmail.com>
2023-03-12 16:55:09 -0400

    updated code for models

---
+++ models/Reaction.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+function dateFormat(timestamp) {
+  return new Date(timestamp).toISOString();
+}
+
+module.exports = ReactionSchema;
