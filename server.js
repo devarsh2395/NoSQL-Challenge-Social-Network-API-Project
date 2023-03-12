8e739e992ecda9637ad538fbbacc43c595cf8ef0
devarsh2395 <devarsh2395@gmail.com>
2023-03-12 10:55:18 -0400

    Updated code for server.js and connection.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const app = express();
+
+const PORT = express.env.port || 3001;
+
+app.use (express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+app.use(require(routes));
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
